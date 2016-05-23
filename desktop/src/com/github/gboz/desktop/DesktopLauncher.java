43082edb8a16b6182816ab45869d2faf5427879a
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-05-23 16:24:58 +0200

    init

---
+++ desktop/src/com/github/gboz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.gboz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.gboz.GameExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameExample(), config);
+	}
+}
