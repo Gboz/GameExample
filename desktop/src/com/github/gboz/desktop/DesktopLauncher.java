b0a90e1deb90bc78c29ec4d9c3a5793d6bc6bd33
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-05-24 17:26:49 +0200

    config desktop launcher

--- desktop/src/com/github/gboz/desktop/DesktopLauncher.java
+++ desktop/src/com/github/gboz/desktop/DesktopLauncher.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.gboz.GameExample;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = GameExample.GAME_NAME;
+		config.width = GameExample.WIDTH;
+		config.height = GameExample.HEIGHT;
+		config.resizable = false;
+
 		new LwjglApplication(new GameExample(), config);
 	}
 }
