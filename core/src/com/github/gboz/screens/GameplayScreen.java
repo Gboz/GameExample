1ef7d2d8ce9cfa979d90dadbc599c7da9ab3166a
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-05-25 16:58:21 +0200

    Displaying Player img

--- core/src/com/github/gboz/screens/GameplayScreen.java
+++ core/src/com/github/gboz/screens/GameplayScreen.java
@@ -1,16 +1,38 @@
 package com.github.gboz.screens;
 
 import com.github.gboz.GameExample;
+import com.github.gboz.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
 
+	private Player player;
+	
 	public GameplayScreen(GameExample game) {
 		super(game);
 		init();
 	}
 
 	private void init() {
+		initPlayer();
+	}
+
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player);
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
 		
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 	}
 
 }
