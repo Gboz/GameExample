6609c9fa8260eb369bf544ff633c21ec2cfab784
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-05-27 16:20:05 +0200

    added player button and click listener

--- core/src/com/github/gboz/screens/GameplayScreen.java
+++ core/src/com/github/gboz/screens/GameplayScreen.java
@@ -1,11 +1,16 @@
 package com.github.gboz.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.github.gboz.GameExample;
 import com.github.gboz.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
 
 	private Player player;
+	private Button playerButton;
 
 	public GameplayScreen(GameExample game) {
 		super(game);
@@ -15,6 +20,27 @@ public class GameplayScreen extends AbstractScreen {
 	@Override
 	protected void init() {
 		initPlayer();
+		initPlayerButton();
+	}
+
+	private void initPlayerButton() {
+		playerButton = new Button(new ButtonStyle());
+		playerButton.setWidth(460);
+		playerButton.setHeight(360);
+		playerButton.setX(10);
+		playerButton.setY(170);
+		playerButton.setDebug(true);
+
+		stage.addActor(playerButton);
+		playerButton.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+				System.out.println("click!");
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
+
 	}
 
 	private void initPlayer() {
