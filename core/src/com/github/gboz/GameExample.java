d8849d3b0cc737a36cd5d8587ae645daa2b2b6bf
Grzegorz Bolesta <gbolesta21@gmail.com>
2016-06-07 16:57:00 +0200

    Added player asset

--- core/src/com/github/gboz/GameExample.java
+++ core/src/com/github/gboz/GameExample.java
@@ -3,9 +3,6 @@ package com.github.gboz;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.github.gboz.screens.SplashScreen;
 
 public class GameExample extends Game {
--- core/src/com/github/gboz/entities/Player.java
+++ core/src/com/github/gboz/entities/Player.java
@@ -8,13 +8,13 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Player extends Image {
-	private final static int WIDTH = 77;
-	private final static int HEIGHT = 152;
+	private final static int WIDTH = 300;
+	private final static int HEIGHT = 300;
 	private final static int STARTING_X = 200;
 	private final static int STARTING_Y = 300;
 
 	public Player() {
-		super(new Texture("ct.jpg"));
+		super(new Texture("player.png"));
 
 		this.setOrigin(WIDTH / 2, HEIGHT / 2);
 		this.setSize(WIDTH, HEIGHT);
